97fed9bdb6641966fd61c29f0cd28af015aea086
choiby777 <choiby777@gmail.com>
2018-08-02 11:42:41 +0900

    Part 3 . BottomNavigationView

---
+++ app/src/main/java/com/cby/benstagram/Util/BottomNavigationViewHelper.java
@@ -0,0 +1,19 @@
+package com.cby.benstagram.Util;
+
+import android.util.Log;
+
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+public class BottomNavigationViewHelper {
+
+    private static final String TAG = "BottomNavigationViewHel";
+
+    public static void setupBottomNavigationView(BottomNavigationViewEx bottomNavigationViewEx){
+        Log.d(TAG, "setupBottomNavigationView: setting up BottomNavigationViewEx");
+
+        bottomNavigationViewEx.enableAnimation(false);
+        bottomNavigationViewEx.enableItemShiftingMode(false);
+        bottomNavigationViewEx.enableShiftingMode(false);
+        bottomNavigationViewEx.setTextVisibility(false);
+    }
+}
