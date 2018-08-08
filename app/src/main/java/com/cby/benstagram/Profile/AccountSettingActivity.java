fbd97f114da3a92c7d1bb27c054993cdce34995d
choiby777 <choiby777@gmail.com>
2018-08-08 18:08:12 +0900

    Part 12 Account Setting Layout에서 Edit Profile / Sign Out 클릭시 해당 Fragement로 전환하는 기능 구현 완료

--- app/src/main/java/com/cby/benstagram/Profile/AccountSettingActivity.java
+++ app/src/main/java/com/cby/benstagram/Profile/AccountSettingActivity.java
@@ -3,17 +3,21 @@ package com.cby.benstagram.Profile;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 
 import com.cby.benstagram.R;
 import com.cby.benstagram.Util.BottomNavigationViewHelper;
+import com.cby.benstagram.Util.SectionStatePagerAdapter;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 import java.util.ArrayList;
@@ -21,19 +25,26 @@ import java.util.ArrayList;
 public class AccountSettingActivity extends AppCompatActivity {
 
     private static final String TAG = "AccountSettingActivity";
-    private Context mContext  = AccountSettingActivity.this;
     private static final int ACTIVITY_NUM = 4;
+    private Context mContext;
+    private ViewPager mViewPager;
+    private RelativeLayout mRelativeLayout;
+    private SectionStatePagerAdapter mSectionStatePagerAdapter;
 
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_setting);
-
+        mContext  = AccountSettingActivity.this;
         Log.d(TAG, "onCreate: starting");
+
+        mViewPager = findViewById(R.id.container);
+        mRelativeLayout = findViewById(R.id.relLayout1);
 
         setupBottomNavigationView();
         setupSettingsList();
+        setupFragments();
 
         ImageView backArrow = findViewById(R.id.backArrow);
         backArrow.setOnClickListener(new View.OnClickListener() {
@@ -46,17 +57,33 @@ public class AccountSettingActivity extends AppCompatActivity {
         });
     }
 
+    private void setupFragments() {
+        mSectionStatePagerAdapter = new SectionStatePagerAdapter(getSupportFragmentManager());
+        mSectionStatePagerAdapter.addFragment(new EditProfileFragment() , getString(R.string.edit_profile_fragment)); // fragment 0
+        mSectionStatePagerAdapter.addFragment(new SignOutFragment() , getString(R.string.sign_out_fragment)); // fragment 1
+    }
+
     private void  setupSettingsList(){
         Log.d(TAG, "setupSettingsList: init 'Account Setting' list ");
 
         ListView listView = findViewById(R.id.lvAccountSettings);
 
         ArrayList<String> options = new ArrayList<>();
-        options.add(getString(R.string.edit_profile));
-        options.add(getString(R.string.sign_out));
+        options.add(getString(R.string.edit_profile_fragment)); // fragment 0
+        options.add(getString(R.string.sign_out_fragment)); // fragment 1
 
         ArrayAdapter adapter = new ArrayAdapter(mContext, android.R.layout.simple_list_item_1, options);
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(TAG, "onItemClick: navigating to fragement : " + position);
+
+                setViewPager(position);
+            }
+        });
     }
 
     /**
@@ -71,5 +98,12 @@ public class AccountSettingActivity extends AppCompatActivity {
         Menu menu = bottomNavigationViewEx.getMenu();
         MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
         menuItem.setChecked(true);
+    }
+
+    public void setViewPager(int fragmentNumber) {
+
+        mRelativeLayout.setVisibility(View.GONE);
+        mViewPager.setAdapter(mSectionStatePagerAdapter);
+        mViewPager.setCurrentItem(fragmentNumber);
     }
 }
