aa01c44b254cc9525a75ebe7cfafece0dc5528c6
choiby777 <choiby777@gmail.com>
2018-08-10 20:59:31 +0900

    Part 14 Edit Profile의 Profile Photo에 이미지 로딩되는 기능 구현

--- app/src/main/java/com/cby/benstagram/Profile/EditProfileFragment.java
+++ app/src/main/java/com/cby/benstagram/Profile/EditProfileFragment.java
@@ -1,23 +1,49 @@
 package com.cby.benstagram.Profile;
 
 import android.os.Bundle;
+import android.print.PrinterId;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.cby.benstagram.R;
+import com.cby.benstagram.Util.UniversalImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoader;
 
 public class EditProfileFragment extends Fragment {
     private static final String TAG = "EditProfileFragment";
+    private ImageView mProfileImage;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
         View view = inflater.inflate(R.layout.fragment_edit_profile , container , false);
 
+        mProfileImage = view.findViewById(R.id.profile_image);
+
+        initImageLoader();
+
+        setProfileImage();
+
         return view;
+    }
+
+    private void setProfileImage() {
+        Log.d(TAG, "setProfileImage: setting Profile Image");
+
+        String imgURL = "tr2.cbsistatic.com/hub/i/r/2017/01/31/7e355c52-c68f-4389-825f-392f2dd2ac19/resize/770x/d19d6c021f770122da649e2a77bd1404/androiddatahero.jpg";
+
+        UniversalImageLoader.setImage(imgURL , mProfileImage , null, "https://");
+    }
+
+    private void initImageLoader() {
+        UniversalImageLoader universalImageLoader = new UniversalImageLoader(getActivity());
+        ImageLoader.getInstance().init(universalImageLoader.getConfig());
     }
 }
--- app/src/main/java/com/cby/benstagram/Util/UniversalImageLoader.java
+++ app/src/main/java/com/cby/benstagram/Util/UniversalImageLoader.java
@@ -1,13 +1,20 @@
 package com.cby.benstagram.Util;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import com.cby.benstagram.R;
 import com.nostra13.universalimageloader.cache.memory.impl.WeakMemoryCache;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.FailReason;
 import com.nostra13.universalimageloader.core.assist.ImageScaleType;
 import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
 
 public class UniversalImageLoader {
 
@@ -15,8 +22,8 @@ public class UniversalImageLoader {
     private static final int defaultImageId = R.drawable.ic_android;
     private Context mContext;
 
-    public UniversalImageLoader(Context mcontext) {
-        this.mContext = mcontext;
+    public UniversalImageLoader(Context context) {
+        this.mContext = context;
     }
 
     public ImageLoaderConfiguration getConfig(){
@@ -39,5 +46,45 @@ public class UniversalImageLoader {
         return imageLoaderConfiguration;
     }
 
+    /**
+     *
+     * @param imageURL
+     * @param imageView
+     * @param progressBar
+     * @param append
+     */
+    public static void setImage(String imageURL , ImageView imageView, final ProgressBar progressBar , String append){
+
+        ImageLoader imageLoader = ImageLoader.getInstance();
+        imageLoader.displayImage(append + imageURL, imageView, new ImageLoadingListener() {
+            @Override
+            public void onLoadingStarted(String imageUri, View view) {
+                if (progressBar != null){
+                    progressBar.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+                if (progressBar != null){
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                if (progressBar != null){
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onLoadingCancelled(String imageUri, View view) {
+                if (progressBar != null){
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+        });
+    }
 
 }
