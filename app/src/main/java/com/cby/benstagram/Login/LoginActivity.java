a15e09932a5fd8bf38ed2cb1f0499d04b9817eaa
choiby777 <choiby777@gmail.com>
2018-08-13 11:31:07 +0900

    Login , Register Activity에 버튼 클릭 이벤트 추가

--- app/src/main/java/com/cby/benstagram/Login/LoginActivity.java
+++ app/src/main/java/com/cby/benstagram/Login/LoginActivity.java
@@ -1,22 +1,49 @@
 package com.cby.benstagram.Login;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import com.cby.benstagram.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
 
     private static final String TAG = "LoginActivity";
+    private Context mContext  = LoginActivity.this;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        setupClickEvents();
 
         Log.d(TAG, "onCreate: Login Activity Created");
+    }
+
+    private void setupClickEvents() {
+        AppCompatButton loginButton = findViewById(R.id.btn_login);
+        loginButton.setOnClickListener(this);
+
+        TextView signUPText = findViewById(R.id.txt_sign_up);
+        signUPText.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int viewId = view.getId();
+
+        if (viewId == R.id.btn_login){
+            finish();
+        }else if (viewId == R.id.txt_sign_up){
+            Intent intent = new Intent(mContext , RegisterActivity.class);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/cby/benstagram/Login/RegisterActivity.java
+++ app/src/main/java/com/cby/benstagram/Login/RegisterActivity.java
@@ -1,13 +1,16 @@
 package com.cby.benstagram.Login;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
 import android.util.Log;
+import android.view.View;
 
 import com.cby.benstagram.R;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener{
 
     private static final String TAG = "RegisterActivity";
 
@@ -16,6 +19,23 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        setupClickEvents();
+
         Log.d(TAG, "onCreate: Register Activity Created");
+    }
+
+    private void setupClickEvents() {
+        AppCompatButton registerButton = findViewById(R.id.btn_register);
+        registerButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int viewId = v.getId();
+
+        if (viewId == R.id.btn_register) {
+            finish();
+        }
     }
 }
