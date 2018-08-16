64881f0d4d58cd2579a4275b490be4337aee0607
choiby777 <choiby777@gmail.com>
2018-08-16 20:18:29 +0900

    Part 30. Profile Activity 에 있던 코드들 일부 Profile Fragment 로 옮김

--- app/src/main/java/com/cby/benstagram/Profile/ProfileFragment.java
+++ app/src/main/java/com/cby/benstagram/Profile/ProfileFragment.java
@@ -1,24 +1,95 @@
 package com.cby.benstagram.Profile;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.cby.benstagram.R;
+import com.cby.benstagram.Util.BottomNavigationViewHelper;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileFragment extends Fragment {
 
     private static final String TAG = "ProfileFragment";
+    private static final int ACTIVITY_NUM = 4;
+
+    private TextView mPosts, mFollowers, mFollowing, mDisplayName, mUserName , mWebSite, mDescription;
+    private ProgressBar mProgressBar;
+    private CircleImageView mProfilePhoto;
+    private GridView gridView;
+    private Toolbar toolbar;
+    private ImageView profileMenu;
+    private BottomNavigationViewEx bottomNavigationView;
+
+    private Context mContext;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile , container , false);
+        Log.d(TAG, "onCreateView: Started");
 
+        mContext = getActivity();
+
+        mPosts = view.findViewById(R.id.txtPostValue);
+        mFollowers = view.findViewById(R.id.txtfollowersValue);
+        mFollowing = view.findViewById(R.id.txtFollowingValue);
+        mDisplayName = view.findViewById(R.id.txt_display_name);
+        mUserName = view.findViewById(R.id.txt_user_name);
+        mWebSite = view.findViewById(R.id.txt_website);
+        mDescription= view.findViewById(R.id.txt_description);
+
+        mProgressBar = view.findViewById(R.id.profileProgressBar);
+        mProfilePhoto = view.findViewById(R.id.profile_image);
+        gridView = view.findViewById(R.id.gridImages);
+        toolbar = view.findViewById(R.id.profileToolbar);
+        profileMenu= view.findViewById(R.id.profileMoreMenu);
+        bottomNavigationView= view.findViewById(R.id.bottomNavViewBar);
+
+        setupToolbar();
+        setupBottomNavigationView();
 
         return view;
     }
+
+    private void setupToolbar(){
+
+        ((ProfileActivity)getActivity()).setSupportActionBar(toolbar);
+
+        profileMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: Profile More Menu clicked");
+
+                Intent intent = new Intent(mContext , AccountSettingActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void setupBottomNavigationView(){
+        Log.d(TAG, "setupBottomNavigationView: setting up BottomNavigationView");
+
+        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationView);
+        BottomNavigationViewHelper.enableNavigation(mContext , bottomNavigationView);
+        Menu menu = bottomNavigationView.getMenu();
+        MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
+        menuItem.setChecked(true);
+    }
+
 }
