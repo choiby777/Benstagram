03e4f1e0f991b12f60406f65ac1bc6e2a68e9aab
choiby777 <choiby777@gmail.com>
2018-08-17 23:43:50 +0900

    Part 34. Edit Profile Fragment에 DB에서 Load한 값 적용

--- app/src/main/java/com/cby/benstagram/Profile/EditProfileFragment.java
+++ app/src/main/java/com/cby/benstagram/Profile/EditProfileFragment.java
@@ -9,15 +9,36 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.cby.benstagram.R;
+import com.cby.benstagram.Util.FirebaseHelper;
 import com.cby.benstagram.Util.UniversalImageLoader;
+import com.cby.benstagram.models.User;
+import com.cby.benstagram.models.UserAccountSettings;
+import com.cby.benstagram.models.UserSettings;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.nostra13.universalimageloader.core.ImageLoader;
 
-public class EditProfileFragment extends Fragment{
+public class EditProfileFragment extends Fragment
+        implements FirebaseAuth.AuthStateListener{
     private static final String TAG = "EditProfileFragment";
+
     private ImageView mProfileImage;
+    private EditText mEditUserName , mEditDisplayName, mEditWebsite,
+            mEditDescription, mEditEmail, mEditPhoneNumber;
+
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mDbReference;
+    private FirebaseHelper mFirebaseHelper;
 
     @Nullable
     @Override
@@ -26,6 +47,13 @@ public class EditProfileFragment extends Fragment{
         View view = inflater.inflate(R.layout.fragment_edit_profile , container , false);
 
         mProfileImage = view.findViewById(R.id.profile_image);
+
+        mEditUserName = view.findViewById(R.id.editText_userName);
+        mEditDisplayName = view.findViewById(R.id.editText_displayName);
+        mEditWebsite = view.findViewById(R.id.editText_website);
+        mEditDescription = view.findViewById(R.id.editText_description);
+        mEditEmail = view.findViewById(R.id.editText_email);
+        mEditPhoneNumber = view.findViewById(R.id.editText_phoneNumber);
 
         setProfileImage();
 
@@ -37,8 +65,53 @@ public class EditProfileFragment extends Fragment{
             }
         });
 
+        setupFirebaseAuth();
+
         return view;
     }
+
+    private void setupFirebaseAuth() {
+        Log.d(TAG, "setupFirebaseAuth: ");
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuth.addAuthStateListener(this);
+
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mDbReference = mFirebaseDatabase.getReference();
+        mFirebaseHelper = new FirebaseHelper(getActivity());
+
+        mDbReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                // retrieve user information from the database
+                UserSettings userSettings = mFirebaseHelper.getUserSettings(dataSnapshot);
+                setProfileWidgets(userSettings);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void setProfileWidgets(UserSettings userSettings){
+
+        User user = userSettings.getUser();
+        UserAccountSettings accountSettings = userSettings.getSetting();
+
+        UniversalImageLoader.setImage(accountSettings.getProfile_photo() , mProfileImage , null , "");
+
+        mEditUserName.setText(user.getUsername());
+        mEditEmail.setText(user.getEmail());
+        mEditPhoneNumber.setText(user.getPhone_number());
+
+        mEditDisplayName.setText(accountSettings.getDisplay_name());
+        mEditWebsite.setText(accountSettings.getWebsite());
+        mEditDescription.setText(accountSettings.getDescription());
+    }
+
 
     private void setProfileImage() {
         Log.d(TAG, "setProfileImage: setting Profile Image");
@@ -46,5 +119,10 @@ public class EditProfileFragment extends Fragment{
         String imgURL = "tr2.cbsistatic.com/hub/i/r/2017/01/31/7e355c52-c68f-4389-825f-392f2dd2ac19/resize/770x/d19d6c021f770122da649e2a77bd1404/androiddatahero.jpg";
 
         UniversalImageLoader.setImage(imgURL , mProfileImage , null, "https://");
+    }
+
+    @Override
+    public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
     }
 }
