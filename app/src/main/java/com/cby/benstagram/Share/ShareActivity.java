a880065668705f03be7545e97a720f0dce442a41
choiby777 <choiby777@gmail.com>
2018-08-28 21:28:33 +0900

    Part 41 ShareActivity에서 필요한 Permission들을 요청하는 기능 구현

--- app/src/main/java/com/cby/benstagram/Share/ShareActivity.java
+++ app/src/main/java/com/cby/benstagram/Share/ShareActivity.java
@@ -1,7 +1,10 @@
 package com.cby.benstagram.Share;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.print.PrinterId;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -9,6 +12,8 @@ import android.view.MenuItem;
 
 import com.cby.benstagram.R;
 import com.cby.benstagram.Util.BottomNavigationViewHelper;
+import com.cby.benstagram.Util.Permissions;
+import com.google.android.gms.flags.IFlagProvider;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 public class ShareActivity extends AppCompatActivity {
@@ -16,6 +21,7 @@ public class ShareActivity extends AppCompatActivity {
     private static final String TAG = "ShareActivity";
     private Context mContext  = ShareActivity.this;
     private static final int ACTIVITY_NUM = 2;
+    private static final int VERIFY_PERMISSIONS_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +30,38 @@ public class ShareActivity extends AppCompatActivity {
 
         Log.d(TAG, "onCreate: starting");
 
+        if (checkPermissionsArray(Permissions.PERMISSIONS)){
+
+        }else{
+            verifyPermission(Permissions.PERMISSIONS);
+        }
+
         setupBottomNavigationView();
+    }
+
+    private void verifyPermission(String[] permissions) {
+        ActivityCompat.requestPermissions(
+                ShareActivity.this,
+                permissions,
+                VERIFY_PERMISSIONS_REQUEST);
+    }
+
+    private boolean checkPermissionsArray(String[] permissions) {
+
+        for (int i=0; i<permissions.length; i++){
+            if (!checkPermissions(permissions[i])){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean checkPermissions(String permission) {
+
+        int permissionRequest = ActivityCompat.checkSelfPermission(ShareActivity.this, permission);
+
+        return permissionRequest == PackageManager.PERMISSION_GRANTED;
     }
 
     /**
---
+++ app/src/main/java/com/cby/benstagram/Util/Permissions.java
@@ -0,0 +1,11 @@
+package com.cby.benstagram.Util;
+
+import android.Manifest;
+
+public class Permissions {
+    public static final String[] PERMISSIONS = {
+            Manifest.permission.CAMERA,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+    };
+}
