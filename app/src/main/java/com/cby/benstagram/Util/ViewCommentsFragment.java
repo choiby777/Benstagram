16549578ae0bd4feca031166970de652f65c4649
choiby777 <choiby777@gmail.com>
2018-10-16 21:57:40 +0900

    setupCommentsEventListener 추가하여 comment 리스트 실시간 추가되도록 구현

--- app/src/main/java/com/cby/benstagram/Util/ViewCommentsFragment.java
+++ app/src/main/java/com/cby/benstagram/Util/ViewCommentsFragment.java
@@ -1,5 +1,6 @@
 package com.cby.benstagram.Util;
 
+import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -12,6 +13,8 @@ import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AlphabetIndexer;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -24,6 +27,7 @@ import com.cby.benstagram.models.Photo;
 import com.cby.benstagram.models.User;
 import com.cby.benstagram.models.UserAccountSettings;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -37,13 +41,18 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import java.util.TimeZone;
+
+import static android.content.Context.INPUT_METHOD_SERVICE;
 
 public class ViewCommentsFragment extends Fragment implements View.OnClickListener {
     private static final String TAG = "ViewCommentsFragment";
 
+    private InputMethodManager inputMethodManager;
     private Photo photo;
     private List<Comment> commentList;
 
@@ -64,18 +73,61 @@ public class ViewCommentsFragment extends Fragment implements View.OnClickListen
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_view_comments, container, false);
+        commentList = new ArrayList<>();
 
-        setupFirebaseAuth();
+        inputMethodManager = (InputMethodManager)getActivity().getSystemService(INPUT_METHOD_SERVICE);
+
+        photo = getPhotoFromBundle();
 
         setupWidgets(view);
 
-        setupCommentList();
+        setupFirebaseAuth();
 
-        photo = getPhotoFromBundle();
+        setupCommentsEventListener();
 
         imgAddComment.setOnClickListener(this);
 
         return view;
+    }
+
+    private void setupCommentsEventListener() {
+        Log.d(TAG, "setupCommentsEventListener: start");
+
+        mDbReference.child(getString(R.string.dbname_photos))
+                .child(photo.getPhoto_id())
+                .child(getString(R.string.field_comments))
+                .addChildEventListener(new ChildEventListener() {
+                    @Override
+                    public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                        Log.d(TAG, "onChildAdded: " + s);
+
+                        Comment comment = dataSnapshot.getValue(Comment.class);
+
+                        commentList.add(comment);
+
+                        setupCommentListAdapter();
+                    }
+
+                    @Override
+                    public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                    }
+
+                    @Override
+                    public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+                    }
+
+                    @Override
+                    public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
     }
 
     private Photo getPhotoFromBundle() {
@@ -97,14 +149,8 @@ public class ViewCommentsFragment extends Fragment implements View.OnClickListen
         mFirebaseHelper = new FirebaseHelper(getActivity());
     }
 
-    private void setupCommentList() {
-        commentList = new ArrayList<>();
-
-        commentList.add(new Comment());
-        commentList.add(new Comment());
-        commentList.add(new Comment());
-
-        CommentListAdapter adapter = new CommentListAdapter(getActivity() , R.layout.layout_comment, commentList);
+    private void setupCommentListAdapter() {
+        final CommentListAdapter adapter = new CommentListAdapter(getActivity() , R.layout.layout_comment, commentList);
         listComments.setAdapter(adapter);
     }
 
@@ -123,7 +169,40 @@ public class ViewCommentsFragment extends Fragment implements View.OnClickListen
             addNewComment();
 
             txtInputComment.setText("");
+            inputMethodManager.hideSoftInputFromWindow(txtInputComment.getWindowToken(),0);
         }
+    }
+
+    private void setupCommentList(){
+        Log.d(TAG, "setupCommentList: start");
+
+        Query query = mDbReference
+                .child(getString(R.string.dbname_photos))
+                .child(photo.getPhoto_id())
+                .child(getString(R.string.field_comments));
+
+        commentList.clear();
+
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Log.d(TAG, "onDataChange: comment count : " + dataSnapshot.getChildrenCount());
+
+                for ( DataSnapshot singleSnapshot : dataSnapshot.getChildren()){
+
+                    Comment comment = singleSnapshot.getValue(Comment.class);
+
+                    commentList.add(comment);
+                }
+
+                setupCommentListAdapter();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.d(TAG, "onCancelled: query canclled");
+            }
+        });
     }
 
     private void addNewComment() {
@@ -148,7 +227,5 @@ public class ViewCommentsFragment extends Fragment implements View.OnClickListen
                 .child(getString(R.string.field_comments))
                 .child(newKey)
                 .setValue(comment);
-
-        commentList.add(comment);
     }
 }
