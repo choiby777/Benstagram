c48f28d06649715abce7479feaa7dd56322551a5
choiby777 <choiby777@gmail.com>
2018-12-22 22:46:37 +0900

    ButterKnife library를 이용하여 message activity의 view들의 바인딩 되도록 구현

--- app/src/main/java/com/cby/benstagram/Message/MessageActivity.java
+++ app/src/main/java/com/cby/benstagram/Message/MessageActivity.java
@@ -4,7 +4,15 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import com.cby.benstagram.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MessageActivity extends AppCompatActivity {
 
@@ -12,11 +20,43 @@ public class MessageActivity extends AppCompatActivity {
 
     private Context mContext  = MessageActivity.this;
 
+    @BindView(R.id.imgBackArrow) ImageView imgBackArrow;
+    @BindView(R.id.imgSearch) ImageView imgSearch;
+    @BindView(R.id.imgMenu) ImageView imgMenu;
+    @BindView(R.id.txtUserName) TextView txtUserName;
+
+    @BindView(R.id.imgInputUser) ImageView imgInputUser;
+    @BindView(R.id.imgSendMessage) ImageView imgSendMessage;
+    @BindView(R.id.txtMessage) EditText txtMessage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
 
         Log.d(TAG, "onCreate: starting");
+
+        ButterKnife.bind(this);
     }
+
+    @OnClick(R.id.imgBackArrow)
+    void onBackArrowImageClick(){
+        Log.d(TAG, "onBackArrowImageClick: ");
+    }
+
+    @OnClick(R.id.imgSearch)
+    void onSearchImageClick(){
+        Log.d(TAG, "onSearchImageClick: ");
+    }
+
+    @OnClick(R.id.imgMenu)
+    void onMenuImageClick(){
+        Log.d(TAG, "onMenuImageClick: ");
+    }
+
+    @OnClick(R.id.imgSendMessage)
+    void onSendMessageClick(){
+        Log.d(TAG, "onSendMessageClick: ");
+    }
+
 }
